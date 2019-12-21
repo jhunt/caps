64898fa0e2e0e911232437ef4522f8eea8697ce5
James Hunt <james@niftylogic.com>
2019-12-21 18:02:40 +0000

    Introspect /proc/self/status by default
    This makes the Docker image 10x more usable.

--- README.md
+++ README.md
@@ -64,3 +64,58 @@ To make the docker image locally:
 To fork and run it under your own DockerHub account:
 
     make IMAGE=yourusername docker push
+
+
+Running Via Docker
+------------------
+
+I provide this as a small (~900k) Docker image, as `huntprod/caps`
+on DockerHub.  There's nothing but this utility in the image, and
+its entrypoint will (without arguments) introspect
+`/proc/self/status` to get the permitted capability set.
+
+Here's some fun stuff to try!
+
+To get your baseline privileges / capabilities:
+
+    →  docker run --rm huntprod/caps
+    (via /proc/self/status)
+    00000000a80425fb (14 capabilities):
+      chown                  0 (0x00000000000001)  Make arbitrary changes to file UIDs and GIDs
+      dac_override           1 (0x00000000000002)  Bypass file read, write, and execute permission checks.
+      fowner                 3 (0x00000000000008)  Bypass file ownership / process owner equality permission checks.
+      fsetid                 4 (0x00000000000010)  Don't clear set-user-ID and set-group-ID mode bits when a file is modified
+      kill                   5 (0x00000000000020)  Bypass permission checks for sending signals.
+      setgid                 6 (0x00000000000040)  Make arbitrary manipulations of process GIDs and supplementary GID list.
+      setuid                 7 (0x00000000000080)  Make arbitrary manipulations of process UIDs.
+      setpcap                8 (0x00000000000100)  Manage capability sets (from bounded / inherited set).
+      net_bind_service      10 (0x00000000000400)  Bind a socket to Internet domain privileged ports.
+      net_raw               13 (0x00000000002000)  Use RAW and PACKET sockets.
+      sys_chroot            18 (0x00000000040000)  Use chroot(2) and manage kernel namespaces.
+      mknod                 27 (0x00000008000000)  Create special files using mknod(2).
+      audit_write           29 (0x00000020000000)  Write records to kernel auditing log.
+      setfcap               31 (0x00000080000000)  Set arbitrary capabilities on a file.
+
+To verify that your `--cap-drop` and `--cap-add` flags are
+working:
+
+    →  docker run --rm --cap-drop all --cap-add net_bind_service --cap-add ipc_lock huntprod/caps
+    (via /proc/self/status)
+    0000000000004400 (2 capabilities):
+      net_bind_service      10 (0x00000000000400)  Bind a socket to Internet domain privileged ports.
+      ipc_lock              14 (0x00000000004000)  Lock memory, via mlock(2) and friends.
+
+Ever tried to mix `--privileged` and `--cap-drop`?  Yeah, it
+doesn't work out so well:
+
+    →  docker run --rm --privileged --cap-drop net_admin huntprod/caps
+    --negate
+    (via /proc/self/status)
+    ~0000003fffffffff (0 capabilities):
+
+What you probably wanted was this:
+
+    →  docker run --rm --cap-add all --cap-drop net_admin huntprod/caps --negate
+    (via /proc/self/status)
+    ~0000003fffffefff (1 capability):
+      net_admin             12 (0x00000000001000)  Manage network configuration and interfaces (broad scope).
--- caps.c
+++ caps.c
@@ -1,5 +1,15 @@
 #include <stdio.h>
 #include <string.h>
+#include <errno.h>
+
+#ifndef PROC_SELF_STATUS
+#  define PROC_SELF_STATUS "/proc/self/status"
+#endif
+
+#ifndef PROC_LINE_SIZE
+#  define PROC_LINE_SIZE 4096
+#endif
+
 #include "caps.inc"
 
 #define cap_name(c)    (CAPS[(c)].name)
@@ -9,6 +19,56 @@
 
 #define for_each_cap(c) for (c = 0; cap_name(c) != NULL; c++)
 #define has(v,c) ((v) & cap_flag(c))
+
+int display(int negate, const char *raw) {
+	unsigned long long v = 0;
+	int x = 0;
+	int i, cap, count;
+	int expected = negate ? 0 : 1;
+
+	for (i = 0; raw[i]; i++) {
+		v = (v << 4);
+		switch (raw[i]) {
+		case '0': case '1': case '2': case '3': case '4':
+		case '5': case '6': case '7': case '8': case '9':
+			v += raw[i] - '0';
+			break;
+
+		case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':
+			v += raw[i] - 'a' + 10;
+			break;
+
+		case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
+			v += raw[i] - 'A' + 10;
+			break;
+
+		case 'x': case 'X':
+			x++;
+			if (x == 1 && v == 0) break;
+		default:
+			fprintf(stderr, "Unrecognized hex literal: '%s'\n", raw);
+			return 2;
+		}
+	}
+
+	count = 0;
+	for_each_cap(cap) {
+		if (has(v, cap) == expected) {
+			count++;
+		}
+	}
+
+	printf("%s%s (%d capabilit%s):\n", (negate ? "~" : ""), raw, count, (count == 1 ? "y" : "ies"));
+	for_each_cap(cap) {
+		if (has(v, cap) == expected) {
+			printf("  %-20s  %2.1d (%#016llx)", cap_name(cap), cap_bit(cap), cap_flag(cap));
+			if (cap_summary(cap)) {
+				printf("  %s", cap_summary(cap));
+			}
+			printf("\n");
+		}
+	}
+}
 
 int main(int argc, char **argv) {
 	int i, j, n, negate;
@@ -23,6 +83,7 @@ int main(int argc, char **argv) {
 
 		if (strcmp(argv[i], "-n") == 0 || strcmp(argv[i], "--negate") == 0) {
 			negate = 1;
+			continue;
 		}
 
 		n++;
@@ -32,53 +93,40 @@ int main(int argc, char **argv) {
 		}
 	}
 
+	if (n == 0) {
+		// pull cap set from /proc/self
+		FILE *in;
+		char *p, buf[PROC_LINE_SIZE];
+
+		in = fopen(PROC_SELF_STATUS, "r");
+		if (!in) {
+			fprintf(stderr, "Unable to open '%s' for reading: %s (error %d)\n", PROC_SELF_STATUS, strerror(errno), errno);
+			return 2;
+		}
+
+		while (fgets(buf, PROC_LINE_SIZE, in) != NULL) {
+			/* 8 = length of "CapPrm:\t" */
+			if (strncmp(buf, "CapPrm:\t", 8) == 0) {
+				fclose(in);
+
+				/* null-terminate the CapPrm line... */
+				for (p = buf; *p != '\r' && *p != '\n'; p++); *p = '\0';
+
+				printf("(via %s)\n", PROC_SELF_STATUS);
+				return display(negate, buf+8);
+			}
+		}
+
+		fprintf(stderr, "Unable to parse '%s' for CapPrm: corrupted /proc/<pid>/status file found\n", PROC_SELF_STATUS);
+		return 2;
+	}
+
 	for (i = 0; i < n; i++) {
-		unsigned long long v = 0;
-		char *raw = argv[i+1];
-		int x = 0;
-		int cap, count;
+		int rc;
 
-		for (j = 0; raw[j]; j++) {
-			v = (v << 4);
-			switch (raw[j]) {
-			case '0': case '1': case '2': case '3': case '4':
-			case '5': case '6': case '7': case '8': case '9':
-				v += raw[j] - '0';
-				break;
-
-			case 'a': case 'b': case 'c': case 'd': case 'e': case 'f':
-				v += raw[j] - 'a' + 10;
-				break;
-
-			case 'A': case 'B': case 'C': case 'D': case 'E': case 'F':
-				v += raw[j] - 'A' + 10;
-				break;
-
-			case 'x': case 'X':
-				x++;
-				if (x == 1 && v == 0) break;
-			default:
-				fprintf(stderr, "Unrecognized hex literal: '%s'\n", raw);
-				return 2;
-			}
-		}
-
-		count = 0;
-		for_each_cap(cap) {
-			if (has(v, cap)) {
-				count++;
-			}
-		}
-
-		printf("%s%s (%d capabilit%s):\n", (negate ? "~" : ""), raw, count, (count == 1 ? "y" : "ies"));
-		for_each_cap(cap) {
-			if (has(v, cap)) {
-				printf("  %-20s  %2.1d (%#016llx)", cap_name(cap), cap_bit(cap), cap_flag(cap));
-				if (cap_summary(cap)) {
-					printf("  %s", cap_summary(cap));
-				}
-				printf("\n");
-			}
+		rc = display(negate, argv[i+1]);
+		if (rc != 0) {
+			return rc;
 		}
 	}
 
